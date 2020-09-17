e5df74854f11a580df6b60cf155fdaee1be1de91
Roman Tomchak <romantomchak@gmail.com>
2020-09-18 01:18:13 +0300

    Upgraded Jane tool version to 6.2. Added php-cs-fixer configuration file

---
+++ src/Runtime/Normalizer/ReferenceNormalizer.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Docker\API\Runtime\Normalizer;
+
+use Jane\JsonSchemaRuntime\Reference;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ReferenceNormalizer implements NormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $ref = [];
+        $ref['$ref'] = (string) $object->getReferenceUri();
+
+        return $ref;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof Reference;
+    }
+}
